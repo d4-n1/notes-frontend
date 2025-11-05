88b0e7d15e12aba002ca7fd3b098fb66b26220c2
d4-n1 <dscasares@gmail.com>
2025-11-05 20:33:23 +0100

    Fix backend URL

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'api/notes';
+const baseUrl = 'https://notes-backend-TU-URL-DE-RENDER.onrender.com/api/notes';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
